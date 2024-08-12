d7cb2877ec43c307afc06949f56270e2135125aa
Mobin <Mobin@DESKTOP-NIS2M55>
2024-08-12 12:14:58 +0330

    Add project files.

---
+++ orange/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel.Design;
+
+namespace orange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstname, lastname, gender = "male", phonenumber, robot;
+            Console.WriteLine("plese enter your first name");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("plaese enter your last name");
+            lastname = Console.ReadLine();
+
+           
+            Console.WriteLine("plaese enter your phonenumber");
+             phonenumber = Console.ReadLine();
+
+            if (phonenumber.StartsWith("09"))
+            {
+
+                Console.WriteLine("Phone number is correct. ");
+            }
+            else
+            {
+                Console.WriteLine("Phone number is not correct .");
+
+
+
+            }
+
+            string userage = default;
+             int age =Convert.ToInt32(userage);
+            
+
+            Console.WriteLine("plaese enter your userage");
+            userage = Console.ReadLine();
+
+            Console.WriteLine("plaese enter your gender");
+            gender = Console.ReadLine();
+
+            if ((gender == "male" && age >= 18) || (gender == "female" && age >= 15))
+            {
+                Console.WriteLine(" you can sign up");
+            }
+            else
+            {
+                Console.WriteLine("you can not sing up");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
